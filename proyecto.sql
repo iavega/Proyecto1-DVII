952046e92def8e25f92f700b8393ac495cf08dc4
Iam Vega <iam.vega@intechideas.com>
2020-11-05 22:32:13 -0500

    update databases

---
+++ proyecto.sql
@@ -0,0 +1,152 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 01-11-2020 a las 03:32:40
+-- Versión del servidor: 10.4.6-MariaDB
+-- Versión de PHP: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyecto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `preguntas`
+--
+
+CREATE TABLE `preguntas` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `preguntas`
+--
+
+INSERT INTO `preguntas` (`id`, `titulo`) VALUES
+(6, '¿Esta es la casa que está en venta?'),
+(7, '¿Tienes el teléfono del mecánico de urgencias?'),
+(8, '¿Leíste el texto que tenías de tarea?'),
+(9, '¿Sabes que vas a estudiar cuando termines el colegio?'),
+(10, '¿Tiene experiencia en el rubro?'),
+(11, '¿Quién fue el primer presidente elegido mediante el voto universal?'),
+(12, '¿Cuál es su estado civil?'),
+(13, '¿Cuándo tendrás vacaciones este verano?'),
+(14, '¿Conoces a mi hermana?'),
+(15, '¿En qué fecha finalizó la Guerra de la Independencia?'),
+(16, 'Debemos presentar Hoy');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `respuestas`
+--
+
+CREATE TABLE `respuestas` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_pregunta` int(11) NOT NULL,
+  `id_respuesta` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `respuestas`
+--
+
+INSERT INTO `respuestas` (`id`, `id_usuario`, `id_pregunta`, `id_respuesta`) VALUES
+(1, 4, 7, 1),
+(2, 4, 8, 1),
+(3, 4, 9, 1),
+(4, 4, 10, 1),
+(5, 4, 11, 1),
+(6, 4, 12, 1),
+(7, 4, 13, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `sexo` int(11) NOT NULL,
+  `edad` int(11) NOT NULL,
+  `salario` int(11) NOT NULL,
+  `provincia` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `sexo`, `edad`, `salario`, `provincia`) VALUES
+(1, 'yessiel', 'sanchez', 2, 75, 20000, 2),
+(2, 'carlos', 'fuentes', 1, 12, 2000, 2),
+(3, 'ovfk', 'vfun', 2, 12, 2000, 1),
+(4, 'vjn', 'vjnfj', 1, 12, 2000, 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `preguntas`
+--
+ALTER TABLE `preguntas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `respuestas`
+--
+ALTER TABLE `respuestas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `preguntas`
+--
+ALTER TABLE `preguntas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT de la tabla `respuestas`
+--
+ALTER TABLE `respuestas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
